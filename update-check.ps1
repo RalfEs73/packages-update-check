4be024d56fb428dedd2acdd1690f5a9eab797fd5
RalfEs73 <ralf@essbaumer.de>
2021-09-28 11:23:35 +0200

    Update update-check.ps1

--- update-check.ps1
+++ update-check.ps1
@@ -11,11 +11,11 @@ $package += ,@('movaviscreenrecorder', 'https://files.movavi.com/x64/MovaviScree
 $package += ,@('movavislideshowmaker', 'https://files.movavi.com/x64/MovaviSlideshowMakerSetup.exe', '9257574C82062730A905F95B23D785A3C41E2FB45DBBDEB8BCD043D4A9B48915')
 $package += ,@('movavivideoconverter', 'https://files.movavi.com/x64/MovaviVideoConverterSetupE.exe', '27841B460FF1907E96060645F854D29FF33275E039163DD736808D8BCAFF54EE')
 $package += ,@('movavivideoeditorplus', 'https://cdn.movavi.com/x64/MovaviVideoEditorPlusSetupE.exe', '312B598DFAE9449BF52A4AE9F249AFC99C1EF837E57F1A3590C255B5523E7084')
-$package += ,@('powerautomatedesktop', 'https://download.microsoft.com/download/b/d/8/bd8409df-7b80-4ef7-89c5-5a7a941a5093/Setup.Microsoft.PowerAutomateDesktop.exe', 'A2C7643044210F9F95683F6C135FA6C6E835C22C15DD2F1528DE36B9DA814C0D')
+$package += ,@('powerautomatedesktop', 'https://download.microsoft.com/download/b/d/8/bd8409df-7b80-4ef7-89c5-5a7a941a5093/Setup.Microsoft.PowerAutomateDesktop.exe', 'C9CD21086DB79D63AABFE0FA8CEC4BD58F662ACBD11C7863D0AD3DCC4F549B16')
 $package += ,@('thumbico', 'https://download.aurelitec.com/thumbico/windows/thumbico-install.exe', '4B7BE6842F95A3252633B30B2AFD292A25D5794FF7C7C1B03B382474B5A3D167')
 $package += ,@('touchportal', 'https://www.touch-portal.com/downloads/TouchPortal_Setup.exe', 'BA07DCC69DD39C98F95E24B60C2B6F39E91E3B9C5224C32FDAE35BE2FE5CE021')
 $package += ,@('droidcam', 'https://files.dev47apps.net/win/DroidCam.Setup.6.4.3.exe', '8E71EAB54503389698604D796C18A3ED9B1B7A67817A7FAD74E86FB1B52EE500')
-$package += ,@('hasleobackupsuite', 'https://www.easyuefi.com/backup-software/downloads/Hasleo_Backup_Suite_Free.exe', 'DBF2A85775B4636C2388A88EC3FD325584FE50B2B83C0759845CC9EA7ADFC297')
+$package += ,@('hasleobackupsuite', 'https://www.easyuefi.com/backup-software/downloads/Hasleo_Backup_Suite_Free.exe', '804AFD42D683938BFB10AD418A5B003DC861C3AF9097BB1E8322273E76404141')
 $package += ,@('tiptoimanager', 'https://www.ravensburger.de/db/installer_flat/tiptoi_Manager_Installer.exe', 'C0BA379CDDB67A1BEC9867170AD42140A994FE60B4FC3F5543F379BCA2BBA49E')
 
 foreach($array in $package)
