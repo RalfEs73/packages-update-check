86e0682a550444d00e24681558999c6b43358f70
RalfEs73 <ralf@essbaumer.de>
2022-04-02 18:04:13 +0200

    Update update-check.ps1

--- update-check.ps1
+++ update-check.ps1
@@ -3,7 +3,7 @@ New-Item -Path "D:\Temp\Cache" -ItemType directory -Force | Out-Null
 $package = @()
 $package += ,@('anystream', 'https://www.redfox.bz/download/SetupAnyStream64.exe', '7FFD789FFA07632EEC911EA9380988A54E30B29FB31AC3BE7D559661D1DE7AF5')
 $package += ,@('encrypto', 'https://dl.devmate.com/com.macpaw.win.Encrypto/EncryptoforWin.exe', '841C55824F88B7FD6D1474ADB8E4F7AA854677DFAF1CE4542768DD99EB73AB59')
-$package += ,@('powerautomatedesktop', 'https://download.microsoft.com/download/b/d/8/bd8409df-7b80-4ef7-89c5-5a7a941a5093/Setup.Microsoft.PowerAutomate.exe', 'BFAD85F97B82FBBBC3F6E5A5DA766525541B60D2131FC4C17D5B4A5B2109D4D8')
+$package += ,@('powerautomatedesktop', 'https://download.microsoft.com/download/b/d/8/bd8409df-7b80-4ef7-89c5-5a7a941a5093/Setup.Microsoft.PowerAutomate.exe', '9E42C7941780DBD0D768A85EC9F0FCB403099707D8299CBF9C867BB71410431C')
 $package += ,@('thumbico', 'https://download.aurelitec.com/thumbico/windows/thumbico-install.exe', '4B7BE6842F95A3252633B30B2AFD292A25D5794FF7C7C1B03B382474B5A3D167')
 $package += ,@('touchportal', 'https://www.touch-portal.com/downloads/TouchPortal_Setup.exe', 'BA07DCC69DD39C98F95E24B60C2B6F39E91E3B9C5224C32FDAE35BE2FE5CE021')
 $package += ,@('droidcam', 'https://files.dev47apps.net/win/DroidCam.Setup.6.4.3.exe', '8E71EAB54503389698604D796C18A3ED9B1B7A67817A7FAD74E86FB1B52EE500')
