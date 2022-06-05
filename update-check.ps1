0c3924a06a3a354612a92117d4ad466ec4a36909
RalfEs73 <ralf@essbaumer.de>
2022-06-05 19:54:13 +0200

    Update update-check.ps1

--- update-check.ps1
+++ update-check.ps1
@@ -3,6 +3,7 @@ New-Item -Path "D:\Temp\Cache" -ItemType directory -Force | Out-Null
 $package = @()
 $package += ,@('anystream.64', 'https://www.redfox.bz/download/SetupAnyStream64.exe', '09293B312CA80DAD2EC08FE9E0E5C35497EDBAAF040F8FEDFB652A3CB83B9BEF')
 $package += ,@('anystream.32', 'https://www.redfox.bz/download/SetupAnyStream32.exe', '7D5A19EB229B3F4C9126B7900DEF25BA4F020E0DA566CC276772309AFFB0FBBF')
+$package += ,@('backupstartmenulayout', 'https://www.sordum.org/files/download/backup-start-menu-layout/BackupSML.zip', '6455092ABCABBA7E46BC933C1909ADBFAEDD57096D0ED0D6ED38439DAF396C81')
 $package += ,@('encrypto', 'https://dl.devmate.com/com.macpaw.win.Encrypto/EncryptoforWin.exe', '841C55824F88B7FD6D1474ADB8E4F7AA854677DFAF1CE4542768DD99EB73AB59')
 $package += ,@('powerautomatedesktop', 'https://download.microsoft.com/download/b/d/8/bd8409df-7b80-4ef7-89c5-5a7a941a5093/Setup.Microsoft.PowerAutomate.exe', '69F0A9997DFCBCA028F59110156A3C0E805EEBAC5FC76DC8B39C16A85A545F29')
 $package += ,@('thumbico', 'https://download.aurelitec.com/thumbico/windows/thumbico-install.exe', '4B7BE6842F95A3252633B30B2AFD292A25D5794FF7C7C1B03B382474B5A3D167')
